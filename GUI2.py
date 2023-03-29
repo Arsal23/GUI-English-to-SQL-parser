b2718d74c90650d3b269a8b21bc22142e621c840
Arsal23 <mohdarslan9395@gmail.com>
2023-03-29 14:12:19 +0530

    first change

---
+++ GUI2.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import time
+from tkinter import *
+import sqlite3
+import subprocess
+from collections import defaultdict as dd
+
+dir = __file__.replace(os.path.basename(__file__), '')
+os.chdir(dir)
+
+db = sqlite3.connect('database.sqlite')
+
+try:
+    parser_exec = os.path.join(dir, sys.argv[1])
+except:
+    parser_exec = os.path.join(dir, "parser")
+
+#defining colours
+base = "yellow"
+white = "#ffffff"
+blue = "#0FB5B5"
+red = "#FF4500"
+grey = "#777777"
+orange = "#FFA400"
+bold = 'bold'
+
+
+window = Tk()
+bg = PhotoImage(file = "bg.png")
+window.title("English to SQL")
+window.configure(bg=base, border=5)
+window.geometry("612x433")
+label1 = Label( window, image = bg)
+label1.place(x = 0,y = 0)
+# def check_parser() -> bool:
+#     var = os.path.exists(parser_exec)
+#     return var
+
+
+def sqlite_exceptions(s):
+    match s:
+        case 'SHOW TABLES;': return "SELECT name FROM sqlite_master WHERE type='table';"
+    return s
+
+
+
+def parse():
+    statement = bytes(input.get() + "\n", 'utf-8')
+    p = subprocess.run(parser_exec, input=statement, capture_output=True)
+    # print(p)
+    if p.returncode != 0:
+        parser_output = "INCORRECT"
+    else:
+        parser_output = p.stdout.decode()
+
+    sql.set(parser_output)
+
+def execute():
+    cur = db.cursor()
+    # print(sqlite_exceptions[sql.get()])
+    s = sqlite_exceptions(sql.get())
+    print(s)
+    cur.execute(s)
+    results = cur.fetchall()
+
+    output.set(results)
+    db.commit()
+    cur.close()
+
+background=Label(window, text='SQL Coverter', bg = base, font=(bold, 20)).pack(pady = 15)
+
+row1 = Frame(window, bg=base)
+input = StringVar(row1)
+
+Label(row1, text='Enter a regular statement: ', bg=base, font=(bold, 12)).pack(side='top', padx=10)
+Entry(row1, textvariable=input, bg=white, width=45, font=bold).pack()
+row1.pack()
+
+Button(window, text="Parse Sentence", font=(bold, 12), command=parse).pack(pady=10)
+
+row2 = Frame(window, bg=base)
+sql = StringVar(row2)
+sql.set("")
+Label(row2, text="Generated SQL statement: ", bg=base, font=(bold, 12)).pack(side='top', padx=10)
+Label(row2, textvariable=sql, bg=white, font=(bold, 12), width=50).pack(side='right', padx=10)
+row2.pack()
+
+row3 = Frame(window, bg=base)
+Label(row3, bg=base, font=(bold, 12)).pack(side='left', padx=10)
+Button(row3, text="SQL Command", font=(bold, 12), command=execute).pack(side='right', padx=10)
+row3.pack(pady=10)
+
+output = StringVar(window)
+Label(window, textvariable=output, width=55, height=20, bg=white, border=10, anchor='nw', padx=10, pady=10, font=(bold, 14)).pack()
+
+
+try:
+    window.mainloop()
+    pass
+finally:
+    db.close()
+
+# print(check_parser())
